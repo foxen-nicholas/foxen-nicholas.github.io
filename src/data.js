04619c990df16657c12ddd41d532ef2ae2449acf
Nick Foxen <foxen.nicholas@gmail.com>
2021-12-16 14:34:51 -0800

    Deploying

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: "Here Comes Treble",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./Sheep.png",
+    link: "https://here-comes-treble.netlify.app/",
+  },
+  {
+    title: "Güdeeds",
+    subtitle: "MERN",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "güddeedsicon.png",
+    link: "https://www.gudeeds.xyz/",
+  },
+  {
+    title: "November Hackathon Project",
+    subtitle: "TBA",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "autumn-leaves.svg",
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/2.jpg",
+    name: "Michael O'Dell",
+    company: "General Assembly",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/3.jpg",
+    name: "Luke Meyers",
+    company: "Pressert Marketing",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "ReactJS",
+  "M.E.R.N.",
+  "NodeJS",
+  "Postgres",
+  "Material UI",
+];
